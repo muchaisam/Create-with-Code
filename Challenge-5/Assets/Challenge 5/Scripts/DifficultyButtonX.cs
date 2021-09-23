679950179a3117daba932d68c1a2aeb93f84aa99
muchaisam <samson.muchai@strathmore.edu>
2021-09-23 12:18:31 +0300

    c5

---
+++ Challenge-5/Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    [SerializeField] private int difficulty = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name);
+
+        GameManagerX gameManagerX = GameObject.FindObjectOfType<GameManagerX>();
+        gameManagerX?.StartGame(difficulty);
+    }
+}
